f6ba09d6f105912842c8d94dd33c644d06d9dadc
srose0040 <115491189+srose0040@users.noreply.github.com>
2023-12-21 16:22:50 -0500

    Attempting to route to pages

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import Onboarding from './pages/Onboarding'
+import {BrowserRouter, Routes, Route} from 'react-route-dom'
 const App = () => {
   return (
     <div>
