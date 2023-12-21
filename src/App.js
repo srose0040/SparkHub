8929d34f5ba92b1ddd17d0be06a4b6a9b604d4f9
srose0040 <115491189+srose0040@users.noreply.github.com>
2023-12-21 16:38:27 -0500

    Successfully set up page routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import Home from './pages/Home'
 import Dashboard from './pages/Dashboard'
 import Onboarding from './pages/Onboarding'
-import {BrowserRouter, Routes, Route} from 'react-route-dom'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 const App = () => {
   return (
-    <div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element ={<Home/>}/>
+        <Route path="/Dashboard" element ={<Dashboard/>}/>
+        <Route path="/Onboarding" element ={<Onboarding/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
