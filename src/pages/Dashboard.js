7742e076cb494283dfbff792db22dd2eb930e54c
srose0040 <115491189+srose0040@users.noreply.github.com>
2023-12-25 21:21:56 -0500

    Implemented and testing swipe feature in code

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -3,7 +3,30 @@ import {useState} from "react";
 
 const Dashboard = () => {
 
-    const characters = db
+    const characters = [
+        {
+            name: 'Richard Hendricks',
+            url: 'https://i.imgur.com/oPj4A8u.jpg'
+        },
+        {
+            name: 'Erlich Bachman',
+            url: 'https://i.imgur.com/oPj4A8u.jpg'
+        },
+        {
+            name: 'Monica Hall',
+            url: 'https://i.imgur.com/oPj4A8u.jpg'
+        },
+        {
+            name: 'Jared Dunn',
+            url: 'https://i.imgur.com/oPj4A8u.jpg'
+        },
+        {
+            name: 'Dinesh Chugtai',
+            url: 'https://i.imgur.com/oPj4A8u.jpg'
+        }
+    ]
+
+
     const [lastDirection, setLastDirection] = useState()
 
     const swiped = (direction, nameToDelete) => {
@@ -18,15 +41,18 @@ const Dashboard = () => {
 
     return (
         <div className="dashboard">
-            <ChatContainer/>
+            {  /* <ChatContainer/> */ }
             <div className="swiper-container">
                 <div className="card-container">
                     {characters.map((character) =>
-                    <TinderCard className='swipe' key={character.name} onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name)}>
+                    <TinderCard
+                        className='swipe'
+                        key={character.name} onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name)}>
                         <div style={{ backgroundImage: 'url(' + character.url + ')' }} className='card'>
                             <h3>{character.name}</h3>
                         </div>
                     </TinderCard>
+                    )}
                 </div>
             </div>
         </div>
