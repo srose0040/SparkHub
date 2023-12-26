87d1d665b602c0bcb45eb009192c46f499090959
srose0040 <115491189+srose0040@users.noreply.github.com>
2023-12-25 20:18:40 -0500

    Beginning work on dashboard. Added react package to project

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,15 @@
+import TinderCard from "react-tinder-card";
+
 const Dashboard = () => {
     return (
-        <div>Dashboard</div>
+        <div className="dashboard">
+            <ChatContainer/>
+            <div className="swiper-container">
+                <div className="card-container">
+
+                </div>
+            </div>
+        </div>
     )
 }
 export default Dashboard
