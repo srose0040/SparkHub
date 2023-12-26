fd0572b731cd88c1acba764f5d0ffc103b766549
srose0040 <115491189+srose0040@users.noreply.github.com>
2023-12-25 20:27:14 -0500

    Added code from package installed to project to facilitate easy creation of code

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,12 +1,32 @@
 import TinderCard from "react-tinder-card";
+import {useState} from "react";
 
 const Dashboard = () => {
+
+    const characters = db
+    const [lastDirection, setLastDirection] = useState()
+
+    const swiped = (direction, nameToDelete) => {
+        console.log('removing: ' + nameToDelete)
+        setLastDirection(direction)
+    }
+/* everything above saves or sets the directions that we went last when swiping */
+
+    const outOfFrame = (name) => {
+        console.log(name + ' left the screen!')
+    }
+
     return (
         <div className="dashboard">
             <ChatContainer/>
             <div className="swiper-container">
                 <div className="card-container">
-
+                    {characters.map((character) =>
+                    <TinderCard className='swipe' key={character.name} onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name)}>
+                        <div style={{ backgroundImage: 'url(' + character.url + ')' }} className='card'>
+                            <h3>{character.name}</h3>
+                        </div>
+                    </TinderCard>
                 </div>
             </div>
         </div>
