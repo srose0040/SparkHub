d0e10ee18ddfa770e6a2889e6a74494dfa4c9c17
srose0040 <115491189+srose0040@users.noreply.github.com>
2024-01-01 19:25:25 -0500

    Set up framework to only allow users to communicate when both of them swipe right

--- Client/src/components/MatchesDisplay.js
+++ Client/src/components/MatchesDisplay.js
@@ -1,11 +1,14 @@
 import axios from "axios";
 import {useEffect, useState} from "react";
+import { useCookies } from "react-cookie";
 
 const MatchesDisplay = ({matches, setClickedUser}) => {
     const [matchedProfiles, setMatchedProfiles] = useState(null)
+    const [cookies, setCookie, removeCookie] = useCookies(null);
 
     /* returns array of all matched user ids */
     const matchedUserIds = matches.map(({user_id}) => user_id)
+    const userId = cookies.UserId;
 
     const getMatches = async () => {
         try {
@@ -23,13 +26,18 @@ const MatchesDisplay = ({matches, setClickedUser}) => {
         getMatches()
     }, [matches])
 
+    const filteredMatchedProfiles = matchedProfiles?.filter(
+        (matchedProfile) =>
+            matchedProfile.matches.filter((profile) => profile.user_id == userId)
+                .length > 0
+    );
 
 
 
 
     return (
         <div className="matches-display">
-            {matchedProfiles?.map((match, _index) =>(
+            {filteredMatchedProfiles?.map((match, _index) =>(
                 <div key={{_index}} className="match-card" onClick={() => setClickedUser(match)}>
                     <div className="img-container">
                         <img src={match?.url} alt={match?.first_name + ' profile'}/>
